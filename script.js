86c1aa4bec36f5367f95f063b6b39ce2228906db
Kizito Azubuike <kayzubi84@gmail.com>
2022-05-19 15:50:34 +0100

    read me button update

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const readMore = document.querySelector('.readmore');
 const dots = document.querySelector('.dots');
 const moreText = document.querySelector('.more');
 
+
+
+readMore.addEventListener('click', () => {
+
+    if(dots.style.display === 'none') {
+        dots.style.display = 'inline-block'
+        readMore.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = 'none'
+        readMore.innerHTML = "Show less";
+        moreText.style.display = "inline";
+    }
+
+})
